62836c56b5b619f5ee245eb4de4732d02dcc5f83
Chris Athanas <realityexpander@gmail.com>
2022-07-16 04:08:29 -0500

    Add ip port for server

--- app/src/main/java/com/realityexpander/guessasketch/data/remote/common/Constants.kt
+++ app/src/main/java/com/realityexpander/guessasketch/data/remote/common/Constants.kt
@@ -8,7 +8,7 @@ object Constants {
     // Rest SketchServer API - ends with a slash //
     ////////////////////////////////////////////////
 
-    //private const val HTTP_BASE_URL_REMOTE = "https://82.180.173.232/" // switch out for server ip
+    //private const val HTTP_BASE_URL_REMOTE = "https://82.180.173.232:8005/" // switch out for server ip
     private const val HTTP_BASE_URL_REMOTE = "https://guess-a-sketch-server.herokuapp.com/"  // for heroku address
     private const val HTTP_BASE_URL_LOCALHOST = "http://192.168.0.186:8005/"  // for hardware device
     private const val HTTP_BASE_URL_EMULATOR = "http://10.0.2.2:8005/" // for emulator
@@ -25,7 +25,7 @@ object Constants {
     // Websockets Drawing API - ends WITHOUT a slash //
     ///////////////////////////////////////////////////
 
-    // private const val WS_BASE_URL_REMOTE = "ws://82.180.173.232/ws/draw"   // for ip-based server
+    // private const val WS_BASE_URL_REMOTE = "ws://82.180.173.232:8005/ws/draw"   // for ip-based server
     private const val WS_BASE_URL_REMOTE = "https://guess-a-sketch-server.herokuapp.com/ws/draw"      // for heroku server
     private const val WS_BASE_URL_LOCALHOST = "ws://192.168.0.186:8005/ws/draw"
     private const val WS_BASE_URL_EMULATOR = "ws://10.0.2.2:8005/ws/draw" // for emulator
